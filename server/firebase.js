5ddfcb3e22afd919a614a74b4a6e3a5a98135bf8
alv-314 <alicov314@gmail.com>
2023-05-09 11:48:17 -0400

    Figuring out firebase config

--- server/firebase.js
+++ server/firebase.js
@@ -1,16 +1,20 @@
-import { initializeApp } from 'firebase/app';
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+
+const dotenv = require('dotenv')
+dotenv.config();
 
 const firebaseConfig = {
-    apiKey: "API_KEY",
-    authDomain: "PROJECT_ID.firebaseapp.com",
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
     // The value of `databaseURL` depends on the location of the database
-    databaseURL: "https://DATABASE_NAME.firebaseio.com",
-    projectId: "PROJECT_ID",
-    storageBucket: "PROJECT_ID.appspot.com",
-    messagingSenderId: "SENDER_ID",
-    appId: "APP_ID",
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
     // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
-    measurementId: "G-MEASUREMENT_ID",
+    measurementId:  process.env.measurementId
 };
 
 const app = initializeApp(firebaseConfig);
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
 const cors = require('cors')
+
 app.use(cors())
 
 const port = process.env.PORT || 8080;
@@ -8,6 +9,15 @@ const port = process.env.PORT || 8080;
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.post('/'), async (req, res) => {
+    const docRef = db.collection('users').doc('alovelace');
+    await docRef.set({
+        first: 'Ada',
+        last: 'Lovelace',
+        born: 1815
+    });
+}
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
