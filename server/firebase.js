6186e61a9b02ac311d99443b8daab1d059aa207f
alv-314 <alicov314@gmail.com>
2023-05-10 08:55:59 -0400

    Successfully verified service account

--- server/firebase.js
+++ server/firebase.js
@@ -1,20 +1,14 @@
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
-const dotenv = require('dotenv')
-dotenv.config();
+const {serviceAccount, firebaseConfig} = require('./config');
 
-const firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    // The value of `databaseURL` depends on the location of the database
-    databaseURL: process.env.databaseURL,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
-    // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
-    measurementId:  process.env.measurementId
-};
+const app = initializeApp({
+  credential: cert(serviceAccount)
+});
 
-const app = initializeApp(firebaseConfig);
+const db = getFirestore();
+
+module.exports = {
+    db
+}
--- server/index.js
+++ server/index.js
@@ -1,24 +1,33 @@
+const {db} = require('./firebase')
+const admin = require('firebase-admin')
+const firebase = require('firebase')
 const express = require('express');
 const app = express()
 const cors = require('cors')
-
 app.use(cors())
+
+const dotenv = require('dotenv')
+dotenv.config();
 
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+    const docRef = db.collection('users').doc('alovelace');
+    try{
+       await docRef.set({
+            first: 'Ada',
+            last: 'Lovelace',
+            born: 1815
+        }); 
+    }catch (e) {
+        console.log(e);
+    }
+    
     res.send('Hello World!')
 })
 
-app.post('/'), async (req, res) => {
-    const docRef = db.collection('users').doc('alovelace');
-    await docRef.set({
-        first: 'Ada',
-        last: 'Lovelace',
-        born: 1815
-    });
-}
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+});
