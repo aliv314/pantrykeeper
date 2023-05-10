d7eb4e7fe4e87e0a64fc38c69d7ac5b744e31e33
alv-314 <alicov314@gmail.com>
2023-05-10 13:42:21 -0400

    Finished register form

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,18 +1,36 @@
+import { useState } from 'react'
 import './Register.scss'
 
 const Register = () => {
-    return (
-        <>
-            <h2> SignUp </h2>
-            <form>
-                <input placeholder='e-Mail'></input>
-                <input placeholder='Username'></input>
-                <input placeholder='Password'></input>
-                <input placeholder='Confirm Password'></input>
+    const [mail, setMail] = useState("");
+    const [user, setUser] = useState("");
+    const [pass, setPass] = useState("");
+    const [confirm, setConfirm] = useState("");
 
-                <button type="submit"/>
+    const onSubmitHandler = (e) =>{
+        e.preventDefault();
+        console.log(mail)
+        console.log(user)
+        console.log(pass)
+        console.log(confirm)
+    }
+
+    return (
+        <section className='register'>
+            <h2 className='register__title'> SignUp </h2>
+            <form className='register__form' onSubmit={onSubmitHandler}>
+                <p className='register__label'>Email</p>
+                <input className='register__input' placeholder='e-Mail' type='text' value={mail} onChange={(e) => setMail(e.target.value)}></input>
+                <p className='register__label'>Username</p>
+                <input className='register__input' placeholder='Username' type='text' value ={user} onChange={(e) => setUser(e.target.value)}></input>
+                <p className='register__label'>Password</p> 
+                <input className='register__input' placeholder='Password' type='password' value ={pass} onChange={(e) => setPass(e.target.value)}></input>
+                <p className='register__label'>Confirm Password</p>
+                <input className='register__input' placeholder='Confirm Password' type='password' value ={confirm} onChange={(e) => setConfirm(e.target.value)}></input>
+
+                <button type="submit"> Submit </button> 
             </form>
-        </>
+        </section>
     )
 }
 
