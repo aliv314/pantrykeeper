f81bab7183d6bc93e701c872422e1bf1f5d30fb0
alv-314 <alicov314@gmail.com>
2023-05-10 18:13:12 -0400

    Started header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,8 +1,12 @@
 import './Header.scss'
-
+import menu from '../../assets/images/icons/menu.svg'
 const Header = () => {
     return (
     <>
+        <div className='header'>
+            <img clasName='header__icon' src={menu} alt='Menu icon header.'/>
+            <h3 className='header__title'> PantryKeeper </h3>
+        </div>
     </>
     )
 }
