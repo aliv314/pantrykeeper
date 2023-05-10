e7822a17ef2502ba7809852b0d7e4ac3565a791b
alv-314 <alicov314@gmail.com>
2023-05-10 10:50:18 -0400

    Changes to the card components in the home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,10 +1,19 @@
 import './Home.scss'
+import HomeCard from '../../components/HomeCard/HomeCard';
+
+import ingredientsIcon from "../../assets/images/icons/nutrition.svg"
+import dishesIcon from '../../assets/images/icons/dinner_dining.svg'
 
 const Home = () => {
     return (
         <>
-            <section className='Hero'>
+            <section className='hero'>
                 <h1>PantryKeeper</h1>
+            </section>
+
+            <section className='home-body'>
+                <HomeCard text = "Ingredients" icon = {ingredientsIcon}/>
+                <HomeCard text = "Dishes" icon = {dishesIcon}/>
             </section>
         </>
     )
