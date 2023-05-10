1cc287014c345948d0cb6312ad3fdb27feb50298
alv-314 <alicov314@gmail.com>
2023-05-10 13:21:07 -0400

    Started register form

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -3,6 +3,15 @@ import './Register.scss'
 const Register = () => {
     return (
         <>
+            <h2> SignUp </h2>
+            <form>
+                <input placeholder='e-Mail'></input>
+                <input placeholder='Username'></input>
+                <input placeholder='Password'></input>
+                <input placeholder='Confirm Password'></input>
+
+                <button type="submit"/>
+            </form>
         </>
     )
 }
