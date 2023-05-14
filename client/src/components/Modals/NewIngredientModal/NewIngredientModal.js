cfdf210f30f3dcd8880f3eda7015be6996de139b
alv-314 <alicov314@gmail.com>
2023-05-14 13:19:48 -0400

    Adding auto-fill and suggest.

--- client/src/components/Modals/NewIngredientModal/NewIngredientModal.js
+++ client/src/components/Modals/NewIngredientModal/NewIngredientModal.js
@@ -1,14 +1,24 @@
 import './NewIngredientModal.scss'
-
+import searchIcon from '../../../assets/images/icons/search.svg'
+import { useState } from 'react'
 const NewIngredientModal = () => {
 
+    const [ingredient, setIngredient] = useState("");
+    const url = `https://api.edamam.com/auto-complete?app_id=${process.env.REACT_APP_EDAMAM_APP_ID}&app_key=${process.env.REACT_APP_EDAMAM_APP_KEY}&q=onion&limit=3`
     return (
         <div className='new-ingredient'>
             <div className='new-ingredient__content'>
-            
+                <h2> Add Ingredient </h2>
+                <p> Search </p>
+                <form className='new-ingredient__form'>
+                    <input className= 'new-ingredient__input' onChange={(e) => setIngredient(e.target.value)} value={ingredient}></input>
+                    <button type='Submit'>Add</button>
+                </form>
+
             </div>
         </div>
     )
 }
 
 export default NewIngredientModal;
+
