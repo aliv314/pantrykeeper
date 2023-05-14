3fc471a83b5a144325b5f4ec6e01b3ed25db33e5
alv-314 <alicov314@gmail.com>
2023-05-14 00:28:46 -0400

    Finished skeleton of ingredients and leftovers controller/routes

--- server/routes/ingredientsRouter.js
+++ server/routes/ingredientsRouter.js
@@ -1,1 +1,14 @@
+const router = require('express').Router();
+const ingredientsRouter = require('../controllers/ingredientsController');
 
+router.route('/:pantry_id')
+.get(ingredientsRouter.getIngredients)
+.post(ingredientsRouter.postIngredient)
+
+router.route('/:pantry_id/:leftover_id')
+.get(ingredientsRouter.getIngredient)
+.put(ingredientsRouter.putIngredient)
+.delete(ingredientsRouter.delIngredient)
+
+
+module.exports = router;
