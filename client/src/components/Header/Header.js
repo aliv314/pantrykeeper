c9bd7559c934654fad8f4c906ea4755c10abc4ec
alv-314 <alicov314@gmail.com>
2023-05-15 10:07:04 -0400

    fixed header styling. header missing case: user not logged in

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,12 +1,31 @@
 import './Header.scss'
 import menu from '../../assets/images/icons/menu.svg'
+import {getAuth, onAuthStateChanged} from "firebase/auth"
+import {useEffect, useState} from 'react'
 
+import profileIcon from '../../assets/images/icons/face.svg'
 const Header = () => {
+
+    const auth = getAuth();
+    const [username, setUsername] = useState("");
+    const [profileImg, setProfileImg] = useState("");
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if(user != null){
+                setUsername(user.displayName);
+                setProfileImg(user.photoURL);
+            }
+        })
+    }, [auth])
+
     return (
     <>
         <div className='header'>
-            <img className='header__icon' src={menu} alt='Menu icon header.'/>
             <h3 className='header__title'> PantryKeeper </h3>
+            <div className='header__user'>
+                <h3> {username}</h3>
+                <img src={profileIcon} alt={`${username}'s profile icon`}/>
+            </div>
         </div>
     </>
     )
