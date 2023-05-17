f310546b463ec216846b0fef068d09be5fe856ee
alv-314 <alicov314@gmail.com>
2023-05-16 22:42:03 -0400

    Styling food details

--- client/src/components/Modals/FoodsModals/FoodDetails/FoodDetails.js
+++ client/src/components/Modals/FoodsModals/FoodDetails/FoodDetails.js
@@ -2,7 +2,7 @@ import './FoodDetails.scss';
 
 import backIcon from '../../../../assets/images/icons/arrow_back.svg'
 import foodIcon from '../../../../assets/images/icons/kitchen.svg';
-
+import BackButton from '../../../BackButton/BackButton';
 const FoodDetails = (props) =>{
     const {show, food, onClose} = props; 
 
@@ -14,21 +14,21 @@ const FoodDetails = (props) =>{
         <div className='food-details'>
             <div className='food-details__content'>
                 <div className='food-details__header'>
-                    <img className='food-details__icon' src={backIcon} alt="Back arrow icon" onClick={onClose}/>
+                    <BackButton onClose={onClose}/>
                     <h2 className='food-details__title'> {food.food_name}</h2>
                 </div>
-                <img className='food-details__icon' src={foodIcon} alt="food icon"></img>
-                <div>
-                    <p> Added by: </p> 
-                    <p> {food.food_name}</p>
+                <img className='food-details__image' src={foodIcon} alt="food icon"></img>
+                <div className='food-details__detail'>
+                    <p className='food-details__label'> Added by: </p> 
+                    <p className='food-details__info'> {food.food_name}</p>
                 </div>
-                <div>
-                    <p> Added on: </p>
-                    <p> {food.timestamp}</p>
+                <div  className='food-details__detail'>
+                    <p className='food-details__label'> Added on: </p>
+                    <p className='food-details__info'> {food.timestamp}</p>
                 </div>
-                <div>
-                    <p> Food Type </p>
-                    <p> {food.food_type}</p>
+                <div  className='food-details__detail'>
+                    <p className='food-details__label'> Food Type </p>
+                    <p className='food-details__info'> {food.food_type}</p>
                 </div>
             </div>
         </div>
