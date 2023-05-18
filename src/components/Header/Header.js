ea5f5028e1cdeab7a46062e69e1aea2671cc636a
alv-314 <alicov314@gmail.com>
2023-05-18 09:19:16 -0400

    Made header auth more consistent

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,28 +8,43 @@ import { useNavigate } from 'react-router-dom'
 const Header = () => {
 
     const auth = getAuth();
+    const [user, setUser] = useState(false)
     const [username, setUsername] = useState("");
     const [profileImg, setProfileImg] = useState("");
     
-    const navigate = useNavigate();
-
-    useEffect(() => {
+    const nav = useNavigate();
+    useEffect(() =>{
         onAuthStateChanged(auth, (user) => {
             if(user != null){
+                setUser(user);
                 setUsername(user.displayName);
                 setProfileImg(user.photoURL);
+            }else{
+                setUser(false)
             }
         })
-    }, [auth])
+        //Will check auth state change after auth loads, or after the window refreshes.
+    }, [auth, window.location.href])
 
+    const loginClickHandler = () => {
+        nav('/login')
+    }
+    const signUpClickHandler = () => {
+        nav('/register')
+    }
     return (
     <>
         <div className='header'>
-            <img src={logo} className='header__title' alt="logo"  onClick={ () => {navigate('/')}} ></img>
+            <img src={logo} className='header__title' alt="logo"  onClick={ () => {nav('/')}} ></img>  
+            {user ? 
             <div className='header__user'>
-                <p className='header__username'> {username? username : "Please Login/Signup"} </p>
+                <p className='header__username'> {username} </p>
                 <img className = 'header__user-icon' src={profileIcon} alt={`${username}'s profile icon`}/>
-            </div>
+            </div> :
+            <div className='header__auth'>
+                <p className='header__login' onClick = {loginClickHandler}> LogIn </p>
+                <p className='header__signup' onClick = {signUpClickHandler}> SignUp </p>
+            </div>}
         </div>
     </>
     )
