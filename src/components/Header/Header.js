e9baccc53c7bf8fd49dca3930cd64d136c99fc97
alv-314 <alicov314@gmail.com>
2023-05-18 05:44:07 -0400

    Made it so clicking the logo takes you to the homepage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,12 +4,15 @@ import {useEffect, useState} from 'react'
 
 import profileIcon from '../../assets/images/icons/face.svg'
 import logo from '../../assets/images/logo/pantrykeeperlogo.png'
+import { useNavigate } from 'react-router-dom'
 const Header = () => {
 
     const auth = getAuth();
     const [username, setUsername] = useState("");
     const [profileImg, setProfileImg] = useState("");
     
+    const navigate = useNavigate();
+
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
             if(user != null){
@@ -22,7 +25,7 @@ const Header = () => {
     return (
     <>
         <div className='header'>
-            <img src={logo} className='header__title' alt="logo"></img>
+            <img src={logo} className='header__title' alt="logo"  onClick={ () => {navigate('/')}} ></img>
             <div className='header__user'>
                 <p className='header__username'> {username} </p>
                 <img className = 'header__user-icon' src={profileIcon} alt={`${username}'s profile icon`}/>
