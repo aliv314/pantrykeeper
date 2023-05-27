289e68bb4fdfe3aa4049b8c72af3865d03677eae
alv-314 <alicov314@gmail.com>
2023-05-27 03:04:53 -0400

    Removed user edit. Irrelevant to app rn

--- src/components/Modals/user-modals/UserDetails/UserDetails.js
+++ src/components/Modals/user-modals/UserDetails/UserDetails.js
@@ -1,31 +1,36 @@
 import './UserDetails.scss'
 
-import BackButton from '../../../BackButton/BackButton';
 import editIcon from '../../../../assets/images/icons/edit.svg'
+import userIcon from '../../../../assets/images/icons/face.svg'
+import BackButton from '../../../BackButton/BackButton';
 
 const UserDetails = (props) =>{
     const {show, user, onClose} = props
-        
+
     if(!show){
         return null;
     }
+    const handleSwitchModal = () =>{
+
+    }
+
     return ( <>
         <div className='user-details' onClick={onClose}>
             <div className='user-details__contents' onClick={(e) => {e.stopPropagation()}} >
                 <div className='user-details__header'>
                     <BackButton onClose={onClose}/>
-                    <h2> Profile: {user && user.displayName}</h2>
-                    <img src={editIcon} alt={'Edit icon'}></img>
+                    <h2 className='user-details__title'> {user.displayName}</h2>
+                    {/* <img onClick ={handleSwitchModal}className='user-details__icon' src={editIcon} alt={'Edit icon'}/> */}
                 </div>
 
-                <img src={''} className='user-details__img' alt='user icon pfp'></img>
+                <img src={userIcon} className='user-details__img' alt='user icon pfp'></img>
                 <div className='user-details__detail'>
                     <p className='user-details__label'> User: </p>
-                    <p className='user-details__info'> {user && user.displayName}</p>
+                    <p className='user-details__info'> {user.displayName}</p>
                 </div>
                 <div className='user-details__detail'>
                     <p className='user-details__label'> FC: </p>
-                    <p className='user-details__info'>  {}</p>
+                    <p className='user-details__info'> Example FC </p>
                 </div>
             </div>
         </div>
