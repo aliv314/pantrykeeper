71aea83efd936ae69ed3a8b31f59823dfe2d37ae
alv-314 <alicov314@gmail.com>
2023-07-09 02:59:10 -0400

    fixed styling on load card

--- src/components/Cards/LoadCard/LoadCard.js
+++ src/components/Cards/LoadCard/LoadCard.js
@@ -3,7 +3,7 @@ import "./LoadCard.scss";
 const LoadCard = (props) => {
   return (
     <>
-      <div className={`load-card`}>
+      <div className="load-card">
         <div className="load-card__body">
           <p className="load-card__title"> Loading </p>
           <div className="lds-ellipsis">
